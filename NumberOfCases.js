23f43783ba3941ec2c8a514bd0091ce422d1057a
viveloper <usik1234@gmail.com>
2020-08-25 22:49:45 +0900

    added permutation, permutation with repetition

--- NumberOfCases.js
+++ NumberOfCases.js
@@ -1,21 +1,64 @@
-function getAllCases(items, n) {
-  const allCases = [];
-  recursion([], 3);
-  return allCases;
+// Permuation with Repetition
+function permuationWithRepetition(items, n) {
+  const result = [];
+  recursion([]);
+  return result;
 
-  function recursion(arr, n) {
+  function recursion(arr) {
     if (arr.length === n) {
-      allCases.push([...arr]);
+      result.push([...arr]);
       return;
     }
     items.forEach((item) => {
       arr.push(item);
-      recursion(arr, n);
+      recursion(arr);
       arr.pop();
     });
   }
 }
 
-const allCases = getAllCases(['a', 'b', 'c'], 3);
+const case01 = permuationWithRepetition(['a', 'b', 'c'], 3);
+console.log(case01, case01.length);
 
-console.log(allCases, allCases.length);
+// Permuation
+function permutation(items, n) {
+  const result = [];
+  recursion(items, []);
+  return result;
+
+  function recursion(restItems, arr) {
+    if (arr.length === n) {
+      result.push([...arr]);
+      return;
+    }
+    restItems.forEach((restItem) => {
+      arr.push(restItem);
+      recursion(
+        restItems.filter((item) => item !== restItem),
+        arr
+      );
+      arr.pop();
+    });
+  }
+}
+
+const case02 = permutation(['a', 'b', 'c'], 3);
+console.log(case02, case02.length);
+
+// Combination with Repetition
+function combinationWithRepetition(items, n) {
+  const result = [];
+  recursion([]);
+  return result;
+
+  function recursion() {}
+}
+
+// Combination
+function combination(items, n) {
+  const result = [];
+  recursion([]);
+  return result;
+
+  function recursion() {}
+}
