83f90449b70602e480e9911fc01807e559837b10
viveloper <usik1234@gmail.com>
2020-08-31 20:58:23 +0900

    added DivideAndConquer

---
+++ Problem-Solving-Patterns/DivideAndConquer.js
@@ -0,0 +1,33 @@
+function naiveSearch(arr, val) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === val) {
+      return i;
+    }
+  }
+  return -1;
+}
+// O(N)
+
+// if sorted array
+function search(arr, val) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (arr[mid] === val) {
+      return mid;
+    } else if (arr[mid] < val) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+}
+// O(log(N))
+
+console.log(search([1, 2, 3, 4, 5, 6], 4));
+console.log(search([1, 2, 3, 4, 5, 6], 6));
+console.log(search([1, 2, 3, 4, 5, 6], 11));
